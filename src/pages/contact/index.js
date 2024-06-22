0fe17a2253928a1c2fe4689c824e466e12fdd78f
Kevin <kevinlane3@hotmail.com>
2024-06-23 00:55:57 +0200

    Now there is a somewhat working Contact Form. The message user sends creates a post in Contentful via Contentfuls Management API.

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from "../../components/layout/layout.js";
 import './contact.css';
 import LinkedIn from '../../components/Icons/LinkedIn.js';
 import GitHub from '../../components/Icons/GitHub.js';
 import { Link, graphql, useStaticQuery } from 'gatsby';
 import { Helmet } from "react-helmet";
+import * as contentful from 'contentful-management';
 
 export default function Index() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
   const data = useStaticQuery(graphql`
     query{
       allContentfulContactLinks {
@@ -15,7 +20,42 @@ export default function Index() {
         }
       }
     }
-  `)
+  `);
+  console.log(data);
+  const handleFirstName = (event) => setFirstName(event.target.value);
+  const handleLastName = (event) => setLastName(event.target.value);
+  const handleEmail = (event) => setEmail(event.target.value);
+  const handleMessage = (event) => setMessage(event.target.value);
+
+  async function submitMessage(){
+    alert("Message sent!");
+
+    let client = contentful.createClient({
+      accessToken: process.env.CONTENTFUL_ACCESS_CMA_TOKEN
+    });
+
+    client.getSpace(process.env.CONTENTFUL_SPACE_ID)
+    .then((space) => space.getEnvironment(process.env.CONTENTFUL_ENVIRONMENT || 'master'))
+    .then((environment) => environment.createEntry('message', {
+      fields: {
+        email: {
+            "en-US": email
+        },
+        firstName: {
+            "en-US": firstName
+        },
+        lastName: {
+            "en-US": lastName
+        },
+        message: {
+            "en-US": message
+        }
+    }
+    }))
+    .then((entry) => console.log(entry))
+    .catch(console.error)
+
+  };
   return (
     <Layout>
       {/* Added Helmet for SEO Reasons */}
@@ -28,19 +68,19 @@ export default function Index() {
         <h3 className='contact-headings'>Contact Me</h3>
         <form className="row g-3">
           <div className="col-md-6">
-            <input type="text" className="form-control" placeholder="First name" />
+            <input type="text" className="form-control" onChange={handleFirstName} placeholder="First name" />
           </div>
           <div className="col-md-6">
-            <input type="text" className="form-control" placeholder="Last name" />
+            <input type="text" className="form-control" onChange={handleLastName} placeholder="Last name" />
           </div>
           <div className="col-12">
-            <input type="text" className="form-control" placeholder="Email" />
+            <input type="text" className="form-control" onChange={handleEmail} placeholder="Email" />
           </div>
           <div className="col-12">
-            <textarea className="form-control"  rows="3"  placeholder='Message'></textarea>
+            <textarea className="form-control"  rows="5" onChange={handleMessage}  placeholder='Message'></textarea>
           </div>
           <div className="d-grid mx-auto">
-            <button id='send-button' type="button" className="btn btn-dark btn-lg">Send</button>
+            <button id='send-button' type="button" onClick={submitMessage} className="btn btn-dark btn-lg">Send</button>
           </div>
         </form>
       </div>
---
+++ src/pages/project/{contentfulCourse.title}.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectPost = () => {
+  return (
+    <div>Blog Post</div>
+  )
+}
+
+export default ProjectPost;
